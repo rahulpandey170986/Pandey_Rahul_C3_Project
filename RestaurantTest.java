1e92e557afe60f1fc9cfe60e1822722b50c9e9f5
rahulpandey170986 <110741344+rahulpandey170986@users.noreply.github.com>
2022-09-07 18:06:44 +0530

    Part 3: Solution

--- RestaurantTest.java
+++ RestaurantTest.java
@@ -13,6 +13,7 @@ class RestaurantTest {
 	LocalTime closingTime = LocalTime.parse("23:00:00");
 	Restaurant restaurant = new Restaurant("Amelie's cafe", "Chennai", openingTime, closingTime);
 
+
 	// >>>>>>>>>>>>>>>>>>>>>>>>>OPEN/CLOSED<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 	// -------FOR THE 2 TESTS BELOW, YOU MAY USE THE CONCEPT OF MOCKING, IF YOU RUN
 	// INTO ANY TROUBLE
@@ -26,7 +27,6 @@ class RestaurantTest {
 	@Test
 	public void is_restaurant_open_should_return_false_if_time_is_outside_opening_and_closing_time() {
 		// WRITE UNIT TEST CASE HERE
-		
 		LocalTime openingTime = LocalTime.parse("16:30:00");
 		LocalTime closingTime = LocalTime.parse("22:00:00");
 		restaurant = new Restaurant("Amelie's cafe", "Chennai", openingTime, closingTime);
@@ -46,6 +46,19 @@ class RestaurantTest {
 		restaurant.addToMenu("Sizzling brownie", 319);
 		assertEquals(initialMenuSize + 1, restaurant.getMenu().size());
 	}
+
+	@Test
+	public void adding_item_from_menu() {
+		restaurant.addToMenu("Sweet corn soup", 119);
+		restaurant.addToMenu("Vegetable lasagne", 269);
+		restaurant.addToMenu("Sizzling brownie", 319);
+		ArrayList<String> newItem = new ArrayList<String>();
+		newItem.add("Vegetable lasagne");
+		newItem.add("Sweet corn soup");
+		Boolean total = restaurant.addingItemFromMenu(newItem);
+		assertTrue(total);
+	}
+
 	@Test
 	public void removing_item_from_menu_should_decrease_menu_size_by_1() throws itemNotFoundException {
 		restaurant.addToMenu("Sweet corn soup", 119);
